0e96a36b730c554fb5c12160b6e9a8cbda86ddc2
0xtomotech <0xtomotech@gmail.com>
2024-05-29 14:36:19 +0200

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/f90a7f8e-10da-4c6d-8cab-ce6c1f92dbea-l6vmp7.jpg",
+  "https://utfs.io/f/7a06787a-9925-4a53-a531-1fd7c57f0c9e-t94uo.jpg",
+  "https://utfs.io/f/0fa7cd49-2550-41b0-af94-b519c62da5d2-twcbo7.jpg",
+  "https://utfs.io/f/a49789b5-64de-4f14-88ec-c5bf07e737b5-s4ehhm.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
