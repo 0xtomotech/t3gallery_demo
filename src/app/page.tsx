aa1a127fa8f7dc8555f55e905df3dcc1476e0276
0xtomotech <0xtomotech@gmail.com>
2024-05-29 15:37:15 +0200

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/f90a7f8e-10da-4c6d-8cab-ce6c1f92dbea-l6vmp7.jpg",
@@ -12,10 +13,18 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} />
