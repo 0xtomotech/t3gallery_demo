eecf14e1ee6cdcf72d279d9a3d8ae4fd02d62a54
0xtomotech <0xtomotech@gmail.com>
2024-05-31 09:31:40 +0200

    Finalize modal and paralel routes

--- README.md
+++ README.md
@@ -13,8 +13,8 @@ The codebase from Theo's [T3 App Router Tutorial on YouTube](https://github.com/
 - [x] Add image upload
 - [x] "taint" (server-only)
 - [x] Use Next/Image component
-- [ ] Error management (w/ Sentry)
-- [ ] Routing/image page (parallel route)
+- [x] Error management (w/ Sentry)
+- [x] Routing/image page (parallel route)
 - [ ] Update upload button to be less cringe
 - [ ] Analytics (posthog)
 - [ ] Delete button (w/ Server Actions)
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,14 +1,26 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
+  const uploaderInfo = await clerkClient.users.getUser(image.userId);
   return (
     <div className="flex h-full w-full min-w-0 text-white">
-      <div className="flex-shrink flex-grow items-center justify-center">
+      <div className="flex flex-shrink items-center justify-center">
         <img src={image.url} className="flex-shrink object-contain" />
       </div>
       <div className="flex h-full w-56 flex-shrink-0 flex-col border-l">
         <div className="border-b p-2 text-center text-xl">{image.name}</div>
+
+        <div className="p-2">
+          <div>Uploaded By:</div>
+          <div>{uploaderInfo.fullName}</div>
+        </div>
+
+        <div className="p-2">
+          <div>Created On:</div>
+          <div>{image.createdAt.toLocaleDateString()}</div>
+        </div>
       </div>
     </div>
   );
